15eb8e76f1d7e44635fa668b9d8d9143d8c7a508
OldHoff <hoffplay8@gmail.com>
2016-10-23 00:39:06 -0500

    Add files via upload

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* local varialbe declaration */
+            int result;
+
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+            return result;
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            /* local varialbe definitino */
+            int a = 100;
+            int b = 200;
+            int ret;
+            NumberManipulator n = new NumberManipulator();
+
+            // calling the FindMax method
+            ret = n.FindMax(a, b);
+            Console.WriteLine("Max value is : {0}", ret);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DefiningConstants/DefiningConstants/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            const double pi2 = Math.PI;
+
+            // constant declaration
+            double r;
+            Console.WriteLine("Enter Radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi * r * r;
+            Console.WriteLine("Readius: {0}, Area: {1}", r, areaCircle);
+            Console.ReadLine();
+
+            // constant declaration actual
+            double r2;
+            Console.WriteLine("Enter Radius: ");
+            r2 = Convert.ToDouble(Console.ReadLine());
+            double areaCircle2 = pi2 * r2 * r2;
+            Console.WriteLine("Readius: {0}, Area: {1}", r2, areaCircle2);
+            Console.ReadLine();
+
+        }
+    }
+}
