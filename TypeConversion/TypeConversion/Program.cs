8c4c9da65a36ff6d1f8ba1dba2787d7fb2b6bd49
OldHoff <hoffplay8@gmail.com>
2016-10-23 00:40:27 -0500

    Add files via upload

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class ExplicitConversion
+    {
+        static void Main(string[] args)
+        {
+            double d = 5673.74;
+            int i;
+
+            // cast double to int.
+            i = (int)d;
+            Console.WriteLine(i);
+            Console.ReadKey();
+            Console.WriteLine("");
+            StringConversion.Main2();
+        }
+    }
+
+    class StringConversion
+    {
+  
+
+        internal static void Main2()
+        {
+            int i = 75;
+            float f = 53.005f;
+            double d = 2345.7652;
+            bool b = true;
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            Console.ReadKey();
+        }
+    }
+}
